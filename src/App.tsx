480cc91ba27b8574734f91682c3dbcd5fda8cf4f
Timon Sommer <timon@timonso.com>
2024-01-21 17:03:50 +0100

    chore: remove styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <RepoFilter _username={username}/>
-      </header>
+      <RepoFilter _username={username}/>
     </div>
   );
 }
